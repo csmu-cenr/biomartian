8e1658e0baabe7215c5790536b243fda96f5ff12
Endre Bakken Stovner <endrebak@idi.ntnu.no>
2015-07-02 13:38:03 +0200

    Can now load modules dynamically

--- biomartian/get_mappings/query_other.py
+++ biomartian/get_mappings/query_other.py
@@ -1,6 +1,7 @@
 
 import pandas as pd
-from os.path import join as path_join
+from os.path import dirname, join as path_join
+from importlib import import_module
 
 from subprocess import call
 
@@ -8,34 +9,11 @@ from biomartian.config.cache_settings import default_cache_path, memory
 
 from widediaper import R
 
-def get_other(intype, outtype, dataset):
+def load_getter(mart, dataset, attribute, basefolder=""):
+    base_module = dirname(basefolder).replace("/", ".")
+    module = "{base_module}.{mart}.{dataset}".format(**locals())
+    dataset_module = import_module(module)
+    attribute_function = getattr(dataset_module,
+                                 "get_{attribute}".format(attribute=attribute))
 
-    if dataset == "go_annotation":
-        return get_go(intype, outtype)
-    elif dataset == "reactome_annotation":
-        return get_reactome()
-
-
-def get_go(intype, outtype, cache_path=default_cache_path):
-
-    # call("mkdir -p {}/human_readable/".format(cache_path), shell=True)
-    r = R()
-
-    r.load_library("GO.db")
-    r("df <- as.data.frame(GOTERM)")
-    r("subset_df <- subset(df, select=c('{}', '{}'))".format(intype, outtype))
-
-    map_df = r.get("subset_df")
-
-    return map_df
-
-
-def get_reactome(pathways="data/ReactomePathways.txt.gz"):
-
-    # Due to bug https://support.bioconductor.org/p/62685/ using text files instead
-
-    pathways_df = pd.read_table(pathways, header=None, names=["reactome",
-                                                              "reactome_annotation"],
-                                compression="infer", usecols=[0, 1])
-
-    return pathways_df
+    return attribute_function
